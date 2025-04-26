4d7650b8e190375908291907a1283dfd26776b72
Braveheart570 <42154318+Braveheart570@users.noreply.github.com>
2025-04-25 21:58:26 -0400

    Basic Canvases

--- main.lua
+++ main.lua
@@ -2,11 +2,30 @@ if os.getenv("LOCAL_LUA_DEBUGGER_VSCODE") == "1" then
     require("lldebugger").start()
   end
 
-print("hello World")
 
- function love.draw()
-    love.graphics.print("Hello World!", 400, 300)
-    end
+
+function love.load()
+    love.window.setMode(800,400)
+    Viewport = love.graphics.newCanvas(400,400)
+    MapView = love.graphics.newCanvas(400,400)
+    
+end
+
+
+function love.draw()
+    love.graphics.setCanvas(Viewport)
+    love.graphics.print("Hello World!", 200, 200)
+    love.graphics.setCanvas(MapView)
+    love.graphics.print("Hello World!", 200, 200)
+    love.graphics.setCanvas()
+    love.graphics.draw(MapView,0,0)
+    love.graphics.draw(Viewport,400,0)
+    
+end
+
+
+function love.update()
+end
 
 
 --sits at the bottom of our script
